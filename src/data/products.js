41844b89e901964b498d1dac38952f85e2b61098
romas <romashka14108@mail.ru>
2023-09-03 00:00:37 +0300

    add product.id

--- src/data/products.js
+++ src/data/products.js
@@ -1,60 +1,72 @@
 export default [
   {
+    id: 1,
     title: 'Жесткий диск 3.5" 2 Tb 5900 rpm 64 Mb cashe Seagate ST2000VX008 SATA III 6 Gb/s',
     price: 12240,
     image: '/img/4_1.jpg'
   },
   {
+    id: 2,
     title: 'Жесткий диск 3.5" 500 Gb 7200 rpm 64 Mb cashe Toshiba HDWD105UZSVA',
     price: 6200,
     image: '/img/4_2.jpg'
   },
   {
+    id: 3,
     title: 'Жесткий диск 3.5" 500 Gb 7200 rpm HP SATA III QK554AA',
     price: 14360,
     image: '/img/4_3.jpg'
   },
   {
+    id: 4,
     title: 'Жесткий диск Seagate Original SATA III 2 Tb ST2000NM000A Exos 7E8 (7200 rpm) 256 Mb 3.5"',
     price: 18690,
     image: '/img/4_4.jpg'
   },
   {
+    id: 5,
     title: 'Умные часы GT 3 MIL-B19S BLACK HUAWEI',
     price: 21810,
     image: '/img/1_3.jpg'
   },
   {
+    id: 6,
     title: 'Умные часы GT 3 MIL-B19V BROWN HUAWEI',
     price: 21810,
     image: '/img/1_4.jpg'
   },
   {
+    id: 7,
     title: 'Умные часы GT RUNNER-B19S BLACK HUAWEI',
     price: 27770,
     image: '/img/1_5.jpg'
   },
   {
+    id: 8,
     title: 'Умные часы GT RUNNER-B19A GRAY HUAWEI',
     price: 27770,
     image: '/img/1_6.jpg'
   },
   {
+    id: 9,
     title: 'Наушники Defender Warhead G-160',
     price: 620,
     image: '/img/5_1.jpg'
   },
   {
+    id: 10,
     title: 'Наушники Apex Pro объёмный звук 7.1, кабель 1.8 м DEFENDER',
     price: 1310,
     image: '/img/5_2.jpg'
   },
   {
+    id: 11,
     title: 'Игровая гарнитура проводная Crown CMGH-2000',
     price: 1500,
     image: '/img/5_3.jpg'
   },
-    {
+  {
+    id: 12,
     title: 'Наушники Sven AP-G333MV',
     price: 870,
     image: '/img/5_4.jpg'
