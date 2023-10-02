d0571720d45f5a7e63d48d00069393985c9d700f
romas <romashka14108@mail.ru>
2023-10-02 11:52:15 +0300

    add vue.config.js and deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# остановить публикацию при ошибках
+set -e
+
+# сборка приложения
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# ини циализация репозитория и загрузка кода в GitHub
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/Dimady/vue-app.git master:gh-pages
+
+cd -
